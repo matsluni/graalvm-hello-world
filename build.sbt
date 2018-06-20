e01abbf468c360c90f0cab75af553295c03caa1a
Gregor Goldmann <gregor.goldmann@commercetools.de>
2018-06-20 18:37:04 +0200

    Simple list sqs queues.

--- build.sbt
+++ build.sbt
@@ -3,3 +3,16 @@ name := "graalvm-hello"
 version := "0.1"
 
 scalaVersion := "2.12.6"
+
+libraryDependencies ++={
+  Seq(
+    "software.amazon.awssdk" % "sqs" % "2.0.0-preview-10"
+  )
+}
+
+assemblyMergeStrategy in assembly := {
+  case x if x.endsWith("META-INF/io.netty.versions.properties") ⇒ MergeStrategy.first
+  case x ⇒
+    val oldStrategy = (assemblyMergeStrategy in assembly).value
+    oldStrategy(x)
+}
--- src/main/scala/de/matsluni/graalvm/Main.scala
+++ src/main/scala/de/matsluni/graalvm/Main.scala
@@ -1,9 +1,22 @@
 package de.matsluni.graalvm
 
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider
+import software.amazon.awssdk.services.sqs.SQSAsyncClient
+
+
 object Main {
 
   def main(args: Array[String]): Unit = {
     println("Hello, world!")
+
+    val client = SQSAsyncClient.builder()
+      .credentialsProvider(ProfileCredentialsProvider.create())
+      .region(software.amazon.awssdk.regions.Region.EU_WEST_1)
+      .build()
+
+    val queues = client.listQueues().join()
+
+    println(queues.queueUrls())
 
   }
 }
