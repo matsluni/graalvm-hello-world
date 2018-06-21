5d32825091f503e6c13426aba7e9b94588d9679e
Matthias Lüneberg <mail@matthias-lueneberg.de>
2018-06-21 16:29:36 +0200

    okhttp works

--- build.sbt
+++ build.sbt
@@ -3,3 +3,9 @@ name := "graalvm-hello"
 version := "0.1"
 
 scalaVersion := "2.12.6"
+
+libraryDependencies ++= {
+  Seq(
+    "com.squareup.okhttp3" % "okhttp" % "3.10.0"
+  )
+}
--- src/main/scala/de/matsluni/graalvm/Main.scala
+++ src/main/scala/de/matsluni/graalvm/Main.scala
@@ -1,9 +1,21 @@
 package de.matsluni.graalvm
 
+import okhttp3.{ConnectionSpec, OkHttpClient, Request}
+
+import scala.collection.JavaConverters._
+
 object Main {
 
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+
+    val client = new OkHttpClient.Builder().connectionSpecs((ConnectionSpec.CLEARTEXT :: Nil).asJava).build()
+    val request = new Request.Builder()
+      .url("http://localhost:9324?Action=ListQueues")
+      .build()
+
+    val response = client.newCall(request).execute()
+
+    println(response.body.string)
 
   }
 }
